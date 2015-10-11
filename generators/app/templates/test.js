c81812c0e3bcfd82727700590bd478e8f4af6882
James Talmage <james@talmage.io>
2015-10-11 03:40:32 -0400

    layout that does not make xo complain

--- generators/app/templates/test.js
+++ generators/app/templates/test.js
@@ -1,14 +1,13 @@
-describe('<%= uncamelName %>', function() {
-  var assert = require('assert');
-  var <%= camelName %> = require('<%= requirePath %>');
-<% if (constructor) { %>
-  var <%= instanceName %>;
-  beforeEach(function(){
-    <%= instanceName %> = new <%= camelName %>();
-  })
-<% } %>
+'use strict';
+var assert = require('assert');
+var <%= camelName %> = require('<%= requirePath %>');
 
-  it('has tests', function() {
+describe('<%= uncamelName %>', function () {<% if (constructor) { %>
+  var <%= instanceName %>;
+  beforeEach(function () {
+    <%= instanceName %> = new <%= camelName %>();
+  });<% } %>
+  it('has tests', function () {
     assert.fail('you need to write tests for <%= uncamelName %>');
   });
 });
